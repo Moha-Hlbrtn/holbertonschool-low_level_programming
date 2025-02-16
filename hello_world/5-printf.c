a7c3f786312bd55cc8e5824e95ebd1cf5c9158ec
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-16 16:32:14 +0100

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée
+ * Description: Affiche un message avec printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art\n");
+    return (0);
+}
