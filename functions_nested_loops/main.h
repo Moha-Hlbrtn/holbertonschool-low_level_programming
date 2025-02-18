4e871cbbaca18bb81f9bf4b7d4755629e18e8ebb
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-18 12:52:19 +0100

    Declaration of the print_alphabet function in the main.h library

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char test); /** Déclaration de la fonction _putchar*/
+int _putchar(char test); /* Déclaration de la fonction _putchar */
+void print_alphabet(void); /* Déclaration de la fonction print_alphabet */
 
 #endif
