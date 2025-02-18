901b8f28fbdf1e35d579717aa1a1e96af1c008b2
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-18 11:59:50 +0100

    task 0

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Déclaration d'un prototype sans paramètres
+ */
+void print_alphabet(void);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar(
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char test); // Déclaration de la fonction _putchar
+
+#endif
