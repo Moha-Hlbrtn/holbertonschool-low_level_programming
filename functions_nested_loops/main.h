1be5db20842bf1f0ee97e9705ca3a3f04df80057
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-18 23:52:07 +0100

    Declaration of print_sign function + coding with it in the C file + including the file used by the checker, to compile it locally + creation of the exe file

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,5 +6,6 @@ void print_alphabet(void); /* Déclaration de la fonction print_alphabet */
 void print_alphabet_x10(void); /* Déclaration de la fonction print_alphabet_x10 */
 int _islower(int c); /* Déclaration de la fonction _islower */
 int _isalpha(int c); /* Déclaration de la fonction _isalpha */
+int print_sign(int n); /* Déclaration de la fonction print_sign */
 
 #endif
