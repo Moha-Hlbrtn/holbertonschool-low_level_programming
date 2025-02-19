905960d9b7178c47de68fe418693aeaa557ea638
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-19 13:54:34 +0100

    task8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - the function
+ * @void: it has no argument
+ * @heures : declaration of the integer for hours
+ * @minutes : declaration of the integer for minutes
+ * return: No return as it has no argument
+ */
+void jack_bauer(void)
+{
+	int heures;
+	int minutes;
+
+	for (heures = 0; heures < 24; heures++)
+	{ for (minutes = 0; minutes < 60; minutes++)
+		{
+		_putchar((heures / 10) + '0');
+		_putchar((heures % 10) + '0');
+		_putchar(':');
+		_putchar((minutes / 10) + '0');
+		_putchar((minutes % 10) + '0');
+		_putchar('\n');
+
+		}
+	}
+}
