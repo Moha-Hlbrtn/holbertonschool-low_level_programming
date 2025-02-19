a9649ef799b3bed84d378d4aca7c5a7dd26d8489
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-19 02:34:29 +0100

    Declaration of the functions till task 9

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,5 +7,9 @@ void print_alphabet_x10(void); /* Déclaration de la fonction print_alphabet_x10
 int _islower(int c); /* Déclaration de la fonction _islower */
 int _isalpha(int c); /* Déclaration de la fonction _isalpha */
 int print_sign(int n); /* Déclaration de la fonction print_sign */
+int _abs(int); /* Déclaration de la fonction _abs */
+int print_last_digit(int); /* Déclaration de la fonction print_last_digit */
+void jack_bauer(void); /* Déclaration de la fonction jack_bauer */
+void times_table(void); /* Déclaration de la fonction times_table */
 
 #endif
