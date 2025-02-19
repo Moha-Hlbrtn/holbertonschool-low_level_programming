dc1571bfa443d3f0e749b4a4b304d80f17cba60f
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-19 11:12:18 +0100

    Declaration of the functions + including the checker c file to test the code locally + creating the exe file

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,20 @@
 #include "main.h"
 
 /**
- * print_last_digit - print the last digit of a number
- * @int: the argument of the function
- *
- * return: 0
+ * print_last_digit - Affiche et retourne le dernier chiffre d'un nombre
+ * @n: Le nombre dont le dernier chiffre doit être extrait
+ * Return: Le dernier chiffre de n
  */
-int print_last_digitr(int)
+int print_last_digit(int n)
 {
-	_putchar(' ');
-	return (0);
+	int last_digit;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+	last_digit = n % 10;
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
