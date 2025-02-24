f8c25a91d3d43023db2fdf99bcba5fd417b2a7ee
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-24 23:13:02 +0100

    Creation of the library + the c file to code + adding the checker c file to test the code locally + creating the executable

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - the function takes a pointer to an int as parameter
+ * @*n: the pointer taken as parameter to updates the value it points to
+ * return: nothing as it is void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c); /* Déclaration de la fonction _putchar */
+void reset_to_98(int *n); /* Déclaration de la fonction reset_to_98 */
+
+#endif
