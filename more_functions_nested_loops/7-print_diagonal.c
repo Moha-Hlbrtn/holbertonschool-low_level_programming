e57266c98bfbb352fb2251bbb62e1aa4f7df4baa
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-24 15:14:33 +0100

    Adding the checker c file to test the code + Creation the file 7 to write the code + creation of its executable

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - the function print n times \ to draw a diagonal line
+ * @n: the number of times it has to be printed
+ * return: no return as it is void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
