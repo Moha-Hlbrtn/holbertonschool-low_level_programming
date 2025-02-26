63fe0e806461a09c4de2ea7cbeb2f2511a84f4d7
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-26 23:28:59 +0100

    Creation of the file to write the code + adding the checker main c file to test it locally + creation of the executable

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer taking into account all the - and +
+ * @s: the string to be converted
+ *
+ * Return: 0 if there are no numbers in the string
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+
+	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return result * sign;
+}
