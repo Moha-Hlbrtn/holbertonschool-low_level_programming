50ba2f981755624424aaeb7fec983a03505a0253
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-26 11:38:18 +0100

    Including all the prototypes to the library

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,5 +5,13 @@ int _putchar(char c); /* Déclaration de la fonction _putchar */
 void reset_to_98(int *n); /* Déclaration de la fonction reset_to_98 */
 void swap_int(int *a, int *b); /* Déclaration de la fonction swap_int */
 int _strlen(char *s); /* Déclaration de la fonction strlen */
+void _puts(char *str); /* Déclaration de la fonction _puts */
+void print_rev(char *s); /* Déclaration de la fonction print_rev */
+void rev_string(char *s); /* Déclaration de la fonction rev_string */
+void puts2(char *str); /* Déclaration de la fonction puts2 */
+void puts_half(char *str); /* Déclaration de la fonction puts_half */
+void print_array(int *a, int n); /* Déclaration de la fonction print_array  */
+char *_strcpy(char *dest, char *src); /* Déclaration de la fonction *_strcpy */
+int _atoi(char *s); /* Déclaration de la fonction _atoi */
 
 #endif
