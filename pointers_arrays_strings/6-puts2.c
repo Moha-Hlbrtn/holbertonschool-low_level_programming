d6f3aefce2d4b7b87a35e1534ddc68f49d1dbc6a
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-26 15:08:28 +0100

    Adding the checker c file to test the code + creating the file to write the code + its executable

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, startin with the first one
+ * @str: the string of which the characters have to be printed
+ * 
+ * Return: nothing as it is void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
