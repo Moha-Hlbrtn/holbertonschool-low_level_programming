0f315b0e1eb9abde0c23e35ae9b36774ab2295ef
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-26 15:11:00 +0100

    Adjusting the code to match the betty style

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, startin with the first one
+ * puts2 - prints every other character of a string, start with 1st one
  * @str: the string of which the characters have to be printed
- * 
  * Return: nothing as it is void
  */
 void puts2(char *str)
