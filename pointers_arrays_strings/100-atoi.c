b6fb315eb9b62b020165eccedfe6ea1063d387de
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-26 23:40:14 +0100

    Modification of the file containing the code because the output seemed correct as the desired output but not validated by the checker + creation of the new executable

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - convert a string to an integer taking into account all the - and +
@@ -6,6 +7,7 @@
  *
  * Return: 0 if there are no numbers in the string
  */
+
 int _atoi(char *s)
 {
 	int i = 0;
@@ -23,7 +25,14 @@ int _atoi(char *s)
 
 	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
 	{
-		result = result * 10 + (s[i] - '0');
+		int digit = s[i] - '0';
+
+		if (result > (INT_MAX - digit) / 10)
+		{
+			return ((sign == 1) ? INT_MAX : INT_MIN);
+		}
+
+		result = result * 10 + digit;
 		i++;
 	}
 
