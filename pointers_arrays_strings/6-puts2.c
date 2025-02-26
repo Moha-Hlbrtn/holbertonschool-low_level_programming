be3d4b76e8f52132d27bc28fb3dfb18d05ed85df
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-26 15:41:52 +0100

    Modification of the code to match the desired output, incremetation + 2 did not work, had to call a previous fonction to count the length of the string + to put a condition that prints only the multiples of 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,23 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
  * puts2 - prints every other character of a string, start with 1st one
- * @str: the string of which the characters have to be printed
+ * @str: the string to be printed
  * Return: nothing as it is void
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
+	int len = _strlen(str);
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (i < len)
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
