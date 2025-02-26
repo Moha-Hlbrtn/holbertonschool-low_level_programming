2ab38579afe67a68a25809ac62a9e34b3a89aabd
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-26 11:33:57 +0100

    Adjusting the code to match the betty style

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * strlen - returns the length of a string
- * @s: the parameter to be returned
- * return: 0 Success
+ * _strlen - Calculate the length of a string
+ * @s: the input string
+ *
+ * Return: an integer, the lenght of the string
  */
 int _strlen(char *s)
 {
 	int len;
-	
+
 	for (len = 0; s[len] != '\0'; len++)
 	{
 	}
-	return len;
+	return (len);
 }
