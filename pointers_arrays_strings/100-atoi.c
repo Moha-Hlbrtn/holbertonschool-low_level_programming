b5edc94aa6687f0b22d925c1be8e4f60114121eb
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-26 23:43:05 +0100

    Modification of the file containing the code because the output seemed correct as the desired output but not validated by the checker + creation of the new executable

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,6 @@
  *
  * Return: 0 if there are no numbers in the string
  */
-
 int _atoi(char *s)
 {
 	int i = 0;
@@ -25,14 +24,7 @@ int _atoi(char *s)
 
 	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
 	{
-		int digit = s[i] - '0';
-
-		if (result > (INT_MAX - digit) / 10)
-		{
-			return ((sign == 1) ? INT_MAX : INT_MIN);
-		}
-
-		result = result * 10 + digit;
+		result = result * 10 + (s[i] - '0');
 		i++;
 	}
 
