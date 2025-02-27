671b89536450d33d616ebe6aa57aa6ed1d1ce76f
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-27 14:17:34 +0100

    Modifying the code because of an error in the output concerning the negative numbers so I added a condition to include them + creation of the executable

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,24 +9,33 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
 	int sign = 1;
-	int result = 0;
+	long num = 0;
+	int started = 0;
 
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	while (*s)
 	{
-		if (s[i] == '-')
+		if (*s == '-')
 		{
 			sign *= -1;
 		}
-		i++;
+		else if (*s >= '0' && *s <= '9')
+		{
+			started = 1;
+			num = num * 10 + (*s - '0');
+			if (num > 2147483647)
+			{
+				if (sign == 1)
+					return (2147483647);
+				else
+					return (-2147483648);
+			}
+		}
+		else if (started)
+		{
+			break;
+		}
+		s++;
 	}
-
-	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
-
-	return (result * sign);
+	return (num * sign);
 }
