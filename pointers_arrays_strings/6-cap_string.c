1cb13ec95ba430aae02bf92b7998ba6002072b4c
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-02-27 14:22:44 +0100

    Creation of the file to write the code + modifying the checker main c file to test it + creation of the executable

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: the string of which the words will be capitalized
+ *
+ * Return: a pointer to the capitalized words of the s string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0') {
+		if (i == 0 || (s[i-1] == ' ' || s[i-1] == '\t' || s[i-1] == '\n' ||
+					s[i-1] == ',' || s[i-1] == ';' || s[i-1] == '.' ||
+					s[i-1] == '!' || s[i-1] == '?' || s[i-1] == '"' ||
+					s[i-1] == '(' || s[i-1] == ')' || s[i-1] == '{' ||
+					s[i-1] == '}')) {
+			if (s[i] >= 'a' && s[i] <= 'z') {
+				s[i] = s[i] - ('a' - 'A');
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
