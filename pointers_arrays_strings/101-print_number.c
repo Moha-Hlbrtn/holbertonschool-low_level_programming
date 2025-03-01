6f6e1110c7a803124932af804d17bbcd9beec03e
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-03-01 21:44:43 +0100

    adjusting the code to match the betty style and the checker output including negative numbers

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -9,17 +9,28 @@ void print_number(int n)
 {
 	int divisor = 1;
 
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		_putchar('1');
+		_putchar('4');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('8');
+		return;
+	}
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-
 	while (n / divisor >= 10)
-	{
 		divisor *= 10;
-	}
-
 	while (divisor > 0)
 	{
 		_putchar((n / divisor) % 10 + '0');
