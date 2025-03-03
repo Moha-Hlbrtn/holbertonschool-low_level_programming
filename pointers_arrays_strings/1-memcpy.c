e34797b4ffd2cb1dec14893c53b60967dab825ea
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-03-03 12:32:20 +0100

    Creating the file to write the code + Adjusting the files 1 & 2 to match the betty style + adding the checker main c file to test the code locally + creating the executable task2

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -4,11 +4,11 @@
  * _memcpy - copies the memory area from one area to an other
  * @src: the memory that will be copied
  * @dest: the destination where the memory will be copied
- * n@: the number of bytes to copy
+ * @n: the number of bytes to copy
  *
  * Return: a pointer to dest
  */
-char *_memcpy(char *dest, char *src, unsigned n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
