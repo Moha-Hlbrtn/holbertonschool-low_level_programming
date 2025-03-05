9b88ca99e0a340093659146f9e84905fb771bd27
Moha-Hlbrtn <mohamed.abbassik@icloud.com>
2025-03-05 12:02:52 +0100

    Creation of the file to write the code + its executable

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: the number of the arguments
+ * @argv: the value of these arguments
+ *
+ * Return: 1 if does not receive 2 arg otherwise the result
+ */
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", num1 * num2);
+	return (0);
+}
